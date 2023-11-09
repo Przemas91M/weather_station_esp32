2a9045fa17a1743401743647e657d8940c0989fd
Przemas91M <przemyslawiec@wp.eu>
2023-11-09 17:16:14 +0100

    Added single database read

---
+++ lib/weather/models/reading.dart
@@ -0,0 +1,37 @@
+import 'package:equatable/equatable.dart';
+
+class StationReading extends Equatable {
+  final double batteryVoltage;
+  final double humidity;
+  final double insideTemperature;
+  final double lux;
+  final double outsideTemperature;
+  final double pressure;
+  final double solarVoltage;
+  final double uvIndex;
+  final String timestamp;
+
+  const StationReading(
+      {required this.batteryVoltage,
+      required this.humidity,
+      required this.insideTemperature,
+      required this.lux,
+      required this.outsideTemperature,
+      required this.pressure,
+      required this.solarVoltage,
+      required this.uvIndex,
+      required this.timestamp});
+
+  @override
+  List<Object?> get props => [
+        batteryVoltage,
+        humidity,
+        insideTemperature,
+        lux,
+        outsideTemperature,
+        pressure,
+        solarVoltage,
+        timestamp,
+        uvIndex
+      ];
+}
