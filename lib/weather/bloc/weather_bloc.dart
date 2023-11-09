3dd302dea6e6bba9d072af4daf738cc2e9ea7085
Przemas91M <przemyslawiec@wp.eu>
2023-11-09 11:20:21 +0100

    Added weather initialisation event

--- lib/weather/bloc/weather_bloc.dart
+++ lib/weather/bloc/weather_bloc.dart
@@ -1,12 +1,21 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather_station_esp32/weather/repository/weather_repo.dart';
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  WeatherBloc({required WeatherRepository weatherRepository})
+      : _weatherRepository = weatherRepository,
+        super(WeatherInitial()) {
+    on<InitializeWeather>(_initializeWeather);
+  }
+  final WeatherRepository _weatherRepository;
+
+  FutureOr<void> _initializeWeather(
+      InitializeWeather event, Emitter<WeatherState> emit) {
+    _weatherRepository.getReadingsOnce('Readings/Koszalin', 5);
   }
 }
--- lib/weather/repository/weather_repo.dart
+++ lib/weather/repository/weather_repo.dart
@@ -1,3 +1,14 @@
 import "package:firebase_database/firebase_database.dart";
 
-class WeatherRepository 
+class WeatherRepository {
+  FirebaseDatabase database = FirebaseDatabase.instance;
+
+  Future<void> getReadingsOnce(String path, int limit) async {
+    final snapshot = await database.ref().child(path).limitToLast(limit).get();
+    if (snapshot.exists) {
+      print(snapshot.value);
+    } else {
+      print('No data available!');
+    }
+  }
+}
