55e036691671afa137d605a3ade24fdad131185f
Przemas91M <przemyslawiec@wp.eu>
2023-11-09 20:05:11 +0100

    Calculate all readings to double

--- lib/weather/repository/weather_repo.dart
+++ lib/weather/repository/weather_repo.dart
@@ -10,18 +10,20 @@ class WeatherRepository {
     if (snapshot.exists) {
       var value =
           Map<String, dynamic>.from(snapshot.value! as Map<Object?, Object?>);
+
       for (var key in value.keys) {
         stationReadings.add(StationReading(
-            batteryVoltage: value[key]['BatteryVoltage'],
-            humidity: value[key]['Humidity'],
-            insideTemperature: value[key]['InsideTemp'],
+            batteryVoltage: value[key]['BatteryVoltage'] * 1.0,
+            humidity: value[key]['Humidity'] * 1.0,
+            insideTemperature: value[key]['InsideTemp'] * 1.0,
             lux: value[key]['Light'] * 1.0,
-            outsideTemperature: value[key]['OutsideTemp'],
-            pressure: value[key]['Pressure'],
-            solarVoltage: value[key]['SolarVoltage'],
+            outsideTemperature: value[key]['OutsideTemp'] * 1.0,
+            pressure: value[key]['Pressure'] * 1.0,
+            solarVoltage: value[key]['SolarVoltage'] * 1.0,
             uvIndex: value[key]['UV'] * 1.0 / 100,
             timestamp: int.parse(key)));
       }
+      stationReadings.sort((a, b) => a.timestamp.compareTo(b.timestamp));
     } else {
       print('No data available!');
     }
