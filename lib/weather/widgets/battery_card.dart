0600be952002376a98e9cf20e357d2594ab94ae6
Przemas91M <przemyslawiec@wp.eu>
2023-11-14 22:15:43 +0100

    Added charge % calculations and formatting

--- lib/weather/widgets/battery_card.dart
+++ lib/weather/widgets/battery_card.dart
@@ -2,24 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:weather_station_esp32/style/color_palette.dart';
 
 class BatteryCard extends StatelessWidget {
-  final String name;
   final double volts;
-  final int charge;
-  final Icon icon;
-  const BatteryCard(
-      {required this.name,
-      required this.volts,
-      required this.icon,
-      required this.charge,
-      super.key});
+  const BatteryCard({required this.volts, super.key});
+  static const Map<String, Color> chargeLevel = {
+    'Full': Colors.green,
+    'Charging': Colors.amber,
+    'Low': Colors.red
+  };
 
   @override
   Widget build(BuildContext context) {
+    int charge = (((volts - 3.2) * 100) ~/ (4.2 - 3.2)).toInt();
+    String level = 'Low';
+    switch (charge) {
+      case >= 80:
+        level = 'Full';
+        break;
+      case < 80 && >= 20:
+        level = 'Charging';
+        break;
+      default:
+        level = 'Low';
+        break;
+    }
     return Container(
       margin:
           const EdgeInsets.only(left: 5.0, bottom: 10.0, right: 5.0, top: 3.0),
-      padding: const EdgeInsets.all(10.0),
-      width: 80,
+      padding: const EdgeInsets.all(15.0),
+      width: 150,
       decoration: BoxDecoration(
           color: Colors.white,
           //border: Border.all(color: ColorPalette.midBlue, width: 3.0),
@@ -35,9 +45,10 @@ class BatteryCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          FittedBox(fit: BoxFit.scaleDown, child: Text(name)),
+          const Text('Battery', style: TextStyle(fontSize: 20)),
           const SizedBox(height: 7.0),
-          icon,
+          Icon(Icons.battery_charging_full_rounded,
+              color: chargeLevel[level], size: 36),
           const SizedBox(height: 7.0),
           Text('$volts V'),
           const SizedBox(height: 7),
