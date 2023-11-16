4cd699d82c3ef41529817cd964abf3d367790a2a
Przemas91M <przemyslawiec@wp.eu>
2023-11-16 21:07:03 +0100

    state keeps readings and forecast

--- lib/weather/bloc/weather_state.dart
+++ lib/weather/bloc/weather_state.dart
@@ -12,8 +12,22 @@ final class WeatherInitial extends WeatherState {}
 final class WeatherLoading extends WeatherState {}
 
 final class WeatherLoadSuccess extends WeatherState {
-  const WeatherLoadSuccess({required this.stationReadings});
+  const WeatherLoadSuccess(
+      {required this.stationReadings,
+      required this.currentWeather,
+      required this.forecastList});
   final List<StationReading> stationReadings;
+  final CurrentWeather currentWeather;
+  final List<Forecast> forecastList;
+}
+
+final class WeatherUpdateLoading extends WeatherState {}
+
+final class WeatherUpdateSuccess extends WeatherState {
+  const WeatherUpdateSuccess(
+      {required this.stationReadings, required this.currentWeather});
+  final List<StationReading> stationReadings;
+  final CurrentWeather currentWeather;
 }
 
 final class WeatherLoadError extends WeatherState {}
