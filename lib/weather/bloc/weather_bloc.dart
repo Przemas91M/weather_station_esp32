9568be05cbf316364424f34aa51b18e1bbc67c8b
Przemas91M <przemyslawiec@wp.eu>
2023-11-16 21:06:16 +0100

    station readings are now stream based

--- lib/weather/bloc/weather_bloc.dart
+++ lib/weather/bloc/weather_bloc.dart
@@ -2,8 +2,12 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather_station_esp32/weather/models/current_weather.dart';
 import 'package:weather_station_esp32/weather/models/reading.dart';
 import 'package:weather_station_esp32/weather/repository/weather_repo.dart';
+
+import '../models/forecast.dart';
+
 part 'weather_event.dart';
 part 'weather_state.dart';
 
@@ -11,21 +15,32 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc({required WeatherRepository weatherRepository})
       : _weatherRepository = weatherRepository,
         super(WeatherLoading()) {
-    on<InitializeWeather>(_initializeWeather);
+    on<StationDataChanged>(_stationDataChanged);
+    //subscribe to database stream - create new stream for each station - maybe do this in a initialisation event?
+    _databaseSubscription = _weatherRepository
+        .databaseDataChanged('Koszalin', 10)
+        .listen((data) => add(StationDataChanged(data: data)));
   }
   final WeatherRepository _weatherRepository;
-  List<StationReading> _stationReadings = [];
+  late final StreamSubscription<List<StationReading>> _databaseSubscription;
 
-  FutureOr<void> _initializeWeather(
-      InitializeWeather event, Emitter<WeatherState> emit) async {
+  FutureOr<void> _stationDataChanged(
+      StationDataChanged event, Emitter<WeatherState> emit) async {
+    //emit(WeatherUpdateLoading()); - think about this!
     emit(WeatherLoading());
-    _weatherRepository.getCurrentWeather('Koszalin');
-    _stationReadings =
-        await _weatherRepository.getReadingsOnce('Readings/Koszalin', 5);
-    if (_stationReadings.isEmpty) {
-      emit(WeatherLoadError());
-    } else {
-      emit(WeatherLoadSuccess(stationReadings: _stationReadings));
-    }
+    CurrentWeather currentWeather =
+        await _weatherRepository.getCurrentWeather('Koszalin');
+    List<Forecast> forecastList =
+        _weatherRepository.getWeatherForecast('Koszalin');
+    emit(WeatherLoadSuccess(
+        stationReadings: event.data,
+        currentWeather: currentWeather,
+        forecastList: forecastList));
+  }
+
+  @override
+  Future<void> close() {
+    _databaseSubscription.cancel();
+    return super.close();
   }
 }
