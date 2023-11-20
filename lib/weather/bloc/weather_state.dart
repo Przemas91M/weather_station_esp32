45bdae5ba4d347592fb91ed55f3a93a5407f9f87
Przemas91M <przemyslawiec@wp.eu>
2023-11-20 09:34:57 +0100

    changed weatherstate to single class with copyWith

--- lib/weather/bloc/weather_state.dart
+++ lib/weather/bloc/weather_state.dart
@@ -1,33 +1,41 @@
 part of 'weather_bloc.dart';
 
-sealed class WeatherState extends Equatable {
-  const WeatherState();
+enum WeatherStatus {
+  initial,
+  loading,
+  loaded,
+  error,
+}
+
+class WeatherState extends Equatable {
+  final WeatherStatus status;
+  final List<StationReading>? stationReadings;
+  final CurrentWeather? currentWeather;
+  final List<Forecast>? weatherForecast;
+  const WeatherState(
+      {required this.status,
+      required this.currentWeather,
+      required this.stationReadings,
+      required this.weatherForecast});
+
+  //initial state constructor
+  static WeatherState initial() => const WeatherState(
+      currentWeather: null,
+      status: WeatherStatus.initial,
+      stationReadings: null,
+      weatherForecast: null);
+
+  WeatherState copyWith(
+          {WeatherStatus? status,
+          CurrentWeather? currentWeather,
+          List<StationReading>? stationReadings,
+          List<Forecast>? weatherForecast}) =>
+      WeatherState(
+          status: status ?? this.status,
+          currentWeather: currentWeather ?? this.currentWeather,
+          stationReadings: stationReadings ?? this.stationReadings,
+          weatherForecast: weatherForecast ?? this.weatherForecast);
 
   @override
   List<Object> get props => [];
 }
-
-final class WeatherInitial extends WeatherState {}
-
-final class WeatherLoading extends WeatherState {}
-
-final class WeatherLoadSuccess extends WeatherState {
-  const WeatherLoadSuccess(
-      {required this.stationReadings,
-      required this.currentWeather,
-      required this.forecastList});
-  final List<StationReading> stationReadings;
-  final CurrentWeather currentWeather;
-  final List<Forecast> forecastList;
-}
-
-final class WeatherUpdateLoading extends WeatherState {}
-
-final class WeatherUpdateSuccess extends WeatherState {
-  const WeatherUpdateSuccess(
-      {required this.stationReadings, required this.currentWeather});
-  final List<StationReading> stationReadings;
-  final CurrentWeather currentWeather;
-}
-
-final class WeatherLoadError extends WeatherState {}
