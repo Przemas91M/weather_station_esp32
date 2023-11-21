597ad03f6e54842d1eb9dde9223a651546331013
Przemas91M <przemyslawiec@wp.eu>
2023-11-21 23:14:18 +0100

    refactoring main file, changed repository provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:weather_station_esp32/auth/repository/auth_repo.dart';
 
 import 'bloc/app.dart';
 
@@ -8,6 +7,5 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  final authRepository = AuthRepository();
-  runApp(App(authRepository: authRepository));
+  runApp(const App());
 }
