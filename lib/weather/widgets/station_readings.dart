52c8241e48b96674ddd8ac9e8d0aa072bf88730e
Przemas91M <przemyslawiec@wp.eu>
2023-11-21 23:21:28 +0100

    widget refactoring

--- lib/weather/widgets/station_readings.dart
+++ lib/weather/widgets/station_readings.dart
@@ -1,44 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:weather_icons/weather_icons.dart';
-import 'package:weather_station_esp32/style/color_palette.dart';
 import 'package:intl/intl.dart';
-import 'package:weather_station_esp32/weather/models/current_weather.dart';
 
-import '../models/reading.dart';
+import '../../style/styling.dart';
+import '../models/models.dart';
 
 class StationReadingsCard extends StatelessWidget {
   const StationReadingsCard(
-      {super.key, required this.reading, required this.currentWeather});
+      {super.key,
+      required this.reading,
+      required this.currentWeather,
+      required this.temperatureUnits});
 
   final StationReading reading;
   final CurrentWeather currentWeather;
+  final bool temperatureUnits;
 
   @override
   Widget build(BuildContext context) {
     //DateTime dateTime = DateTime.now();
     DateTime readTimestamp = DateTime.fromMillisecondsSinceEpoch(
         (reading.timestamp /*+ dateTime.timeZoneOffset.inSeconds)*/ * 1000));
+    //Fahrenheits
     return Container(
       padding: const EdgeInsets.all(10.0),
       margin: const EdgeInsets.only(top: 15),
       decoration: BoxDecoration(
           color: Colors.white,
-          border: Border.all(
-            color: ColorPalette.lightBlue,
-            width: 1.0,
-          ),
-          borderRadius: BorderRadius.circular(15.0),
-          boxShadow: const [
-            BoxShadow(
-                color: ColorPalette.lightBlue,
-                offset: Offset(3.0, 3.0),
-                blurRadius: 0.0,
-                blurStyle: BlurStyle.outer),
-            /*BoxShadow(
-              color: ColorPalette.lightBlue,
-              offset: Offset(5.0, 5.0),*/
-            //)
-          ]),
+          border: CardStyle.thinBorder(),
+          borderRadius: BorderRadius.circular(15.0)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -48,9 +38,7 @@ class StationReadingsCard extends StatelessWidget {
             'Current weather',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
           ),
-          const SizedBox(height: 5),
-          const Divider(color: Colors.black, height: 2.0),
-          const SizedBox(height: 10),
+          const Divider(color: Colors.black, height: 10.0),
           Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
             //TODO Wrap this with Gesture Detector and show details after tap
             //(show graph with reading history!)
@@ -61,7 +49,8 @@ class StationReadingsCard extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('${reading.insideTemperature}°C',
+                  Text(
+                      '${temperatureUnits ? reading.insideTemperature.toStringAsFixed(1) : reading.insideTemperatureF.toStringAsFixed(1)}${temperatureUnits ? '°C' : 'F'}',
                       style: const TextStyle(fontSize: 24.0)),
                   const SizedBox(height: 10),
                   BoxedIcon(
@@ -93,7 +82,7 @@ class StationReadingsCard extends StatelessWidget {
                         children: [
                           const BoxedIcon(WeatherIcons.humidity,
                               color: ColorPalette.yellow),
-                          Text('${reading.humidity.toStringAsFixed(1)} %')
+                          Text('${reading.humidity.toStringAsFixed(1)}%')
                         ]),
                     const SizedBox(height: 5),
                     // pressure reading
