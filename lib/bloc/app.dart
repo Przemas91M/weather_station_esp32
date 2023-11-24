b22acaf3581859411cccf6620059afd56e3a94f3
Przemas91M <przemyslawiec@wp.eu>
2023-11-24 21:16:01 +0100

    added theme and locale data

--- lib/bloc/app.dart
+++ lib/bloc/app.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:weather_station_esp32/auth/repository/auth_repo.dart';
 import 'package:weather_station_esp32/auth/view/sign_in_page.dart';
 import 'package:weather_station_esp32/auth/view/sign_up_page.dart';
 import 'package:weather_station_esp32/settings/repository/settings_repo.dart';
 import 'package:weather_station_esp32/settings/view/settings_screen.dart';
-import 'package:weather_station_esp32/style/color_palette.dart';
+import 'package:weather_station_esp32/style/styling.dart';
 import 'package:weather_station_esp32/weather/repository/weather_repo.dart';
 import 'package:weather_station_esp32/weather/view/weather_main_page.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../settings/bloc/settings_bloc.dart';
 import 'app_bloc.dart';
 
@@ -48,6 +49,16 @@ class AppView extends StatelessWidget {
         context.watch<SettingsBloc>().settingsMap['darkTheme'] ?? false;
     return MaterialApp(
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'), // English
+          Locale('pl'), // Polish
+        ],
         routes: {
           '/signIn': (context) => const SignInPage(),
           '/signUp': (context) => const SignUpPage(),
@@ -55,10 +66,77 @@ class AppView extends StatelessWidget {
           '/settings': (context) => const SettingsScreen()
         },
         theme: ThemeData(
-            textTheme: const TextTheme(
-                bodyLarge:
-                    TextStyle(color: ColorPalette.midBlue, fontSize: 20.0),
-                bodyMedium: TextStyle(color: ColorPalette.midBlue))),
+            useMaterial3: true,
+            brightness: Brightness.light,
+            colorScheme: const ColorScheme.light().copyWith(
+                primary: ColorPalette.lightBlue,
+                background: Colors.grey.shade300,
+                outline: ColorPalette.lightBlue,
+                secondary: ColorPalette.lightBlue,
+                surface: Colors.white),
+            iconTheme: const IconThemeData(color: ColorPalette.lightBlue),
+            dividerColor: Colors.grey,
+            appBarTheme: AppBarTheme(
+                surfaceTintColor: Colors.white,
+                backgroundColor: Theme.of(context).colorScheme.surface,
+                elevation: 5.0,
+                shadowColor: ColorPalette.lightBlue,
+                iconTheme: const IconThemeData(color: ColorPalette.lightBlue),
+                titleTextStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500)),
+            textTheme: const TextTheme().copyWith(
+              //text used in card titles, bold text
+              displayMedium:
+                  const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+              //text used to show big temperature readings
+              displayLarge: const TextStyle(
+                  fontSize: 24.0, fontWeight: FontWeight.normal),
+              //text used to show condition text,
+              displaySmall: const TextStyle(fontSize: 20.0),
+              //normal text to show readings
+              bodyLarge: const TextStyle(fontWeight: FontWeight.w500),
+              bodyMedium:
+                  const TextStyle(fontWeight: FontWeight.w500, fontSize: 14.0),
+            )),
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.dark,
+          iconTheme: const IconThemeData(color: Colors.white),
+          colorScheme: const ColorScheme.dark().copyWith(
+              primary: Colors.black,
+              secondary: Colors.white,
+              outline: Colors.white,
+              background: Colors.blueGrey.shade900),
+          appBarTheme: const AppBarTheme(
+              surfaceTintColor: Colors.black,
+              shadowColor: Colors.white,
+              elevation: 5.0,
+              titleTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500)),
+          dividerColor: Colors.white,
+          textTheme: const TextTheme().copyWith(
+              //text used in card titles, bold text
+              displayMedium: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500),
+              //text used to show big temperature readings
+              displayLarge: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.normal),
+              //text used to show condition text,
+              displaySmall:
+                  const TextStyle(color: Colors.white, fontSize: 20.0),
+              //normal text to show readings
+              bodyLarge: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w500),
+              bodySmall: const TextStyle(color: Colors.white)),
+        ),
         themeMode: theme ? ThemeMode.dark : ThemeMode.light,
         home: BlocBuilder<AppBloc, AppState>(builder: (context, state) {
           if (state is Authenticated) {
