f4cbc722db3318f75883d921e2cf2215e4ae3782
Przemas91M <przemyslawiec@wp.eu>
2023-11-28 20:33:05 +0100

    new cubit to manage user saved locations

--- lib/locations_management/cubit/location_management_cubit.dart
+++ lib/locations_management/cubit/location_management_cubit.dart
@@ -1,8 +1,65 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather_station_esp32/weather/repository/weather_repo.dart';
+
+import '../models/location.dart';
 
 part 'location_management_state.dart';
 
 class LocationManagementCubit extends Cubit<LocationManagementState> {
-  LocationManagementCubit() : super(LocationManagementInitial());
+  final WeatherRepository _weatherRepository;
+  LocationManagementCubit({required weatherRepository})
+      : _weatherRepository = weatherRepository,
+        super(LocationManagementState.initial());
+
+  Future<void> getSavedLocations(String uid) async {
+    emit(state.copyWith(status: LocationStatus.loading));
+    List<Location> locations =
+        await _weatherRepository.getUserSavedLocations(uid);
+    emit(state.copyWith(
+        status: LocationStatus.loaded, savedLocations: locations));
+  }
+
+  void reorderLocationsList(int oldIndex, int newIndex) {
+    emit(state.copyWith(status: LocationStatus.loading));
+    List<Location> locations = state.savedLocations;
+    if (locations.isNotEmpty) {
+      if (newIndex >= locations.length) {
+        newIndex -= 1;
+      }
+      Location item = locations.removeAt(oldIndex);
+      locations.insert(newIndex, item);
+      emit(state.copyWith(
+          status: LocationStatus.loaded, savedLocations: locations));
+    }
+  }
+
+  void removeLocationFromList(int index, String uid) {
+    emit(state.copyWith(status: LocationStatus.loading));
+    List<Location> locations = state.savedLocations;
+    if (locations.isNotEmpty) {
+      locations.removeAt(index);
+      _weatherRepository.saveUserSavedLocations(uid, locations);
+      emit(state.copyWith(
+          status: LocationStatus.loaded, savedLocations: locations));
+    }
+  }
+
+  void addAndSaveLocations(Location location, String uid) {
+    List<Location> locations = state.savedLocations.toList();
+    locations.add(location);
+    emit(state.copyWith(savedLocations: locations));
+    saveLocationsToDataBase(uid);
+  }
+
+  void saveLocationsToDataBase(String uid) async {
+    emit(state.copyWith(status: LocationStatus.loading));
+    bool done = await _weatherRepository.saveUserSavedLocations(
+        uid, state.savedLocations);
+    done
+        ? emit(state.copyWith(status: LocationStatus.saved))
+        : emit(state.copyWith(
+            status: LocationStatus.error,
+            errorMessage: 'Error while saving locations!'));
+  }
 }
--- lib/locations_management/cubit/location_management_state.dart
+++ lib/locations_management/cubit/location_management_state.dart
@@ -1,10 +1,28 @@
 part of 'location_management_cubit.dart';
 
-sealed class LocationManagementState extends Equatable {
-  const LocationManagementState();
+enum LocationStatus { initial, loading, loaded, error, saved }
+
+class LocationManagementState extends Equatable {
+  final LocationStatus status;
+  final List<Location> savedLocations;
+  final String errorMessage;
+  const LocationManagementState(
+      {required this.status,
+      required this.savedLocations,
+      required this.errorMessage});
+
+  static LocationManagementState initial() => const LocationManagementState(
+      status: LocationStatus.initial, savedLocations: [], errorMessage: '');
+
+  LocationManagementState copyWith(
+          {LocationStatus? status,
+          List<Location>? savedLocations,
+          String? errorMessage}) =>
+      LocationManagementState(
+          status: status ?? this.status,
+          savedLocations: savedLocations ?? this.savedLocations,
+          errorMessage: errorMessage ?? this.errorMessage);
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [status, savedLocations, errorMessage];
 }
-
-final class LocationManagementInitial extends LocationManagementState {}
